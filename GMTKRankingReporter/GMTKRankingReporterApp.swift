c824e9d305bf2ff44463c293fc18b9047b90858e
KultrasHub <s3863956@rmit.edu.vn>
2022-07-27 13:50:00 +0700

    Initial Commit

---
+++ GMTKRankingReporter/GMTKRankingReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMTKRankingReporterApp.swift
+//  GMTKRankingReporter
+//
+//  Created by Khoa on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GMTKRankingReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
